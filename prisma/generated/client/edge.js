cb7c77dc32dd2207d5f0c69c9671019d01d175b9
Rodrigo <rodrigo.rm123.rm@gmail.com>
2024-09-22 19:35:57 -0300

    setting prisma to vercel

--- prisma/generated/client/edge.js
+++ prisma/generated/client/edge.js
@@ -136,8 +136,7 @@ const config = {
     "isCustomOutput": true
   },
   "relativeEnvPaths": {
-    "rootEnvPath": null,
-    "schemaEnvPath": "../../../.env"
+    "rootEnvPath": null
   },
   "relativePath": "../..",
   "clientVersion": "5.19.1",
--- prisma/generated/client/index.js
+++ prisma/generated/client/index.js
@@ -137,8 +137,7 @@ const config = {
     "isCustomOutput": true
   },
   "relativeEnvPaths": {
-    "rootEnvPath": null,
-    "schemaEnvPath": "../../../.env"
+    "rootEnvPath": null
   },
   "relativePath": "../..",
   "clientVersion": "5.19.1",
